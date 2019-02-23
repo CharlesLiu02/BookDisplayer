bd6f9687ddc6e2b1ef0f100cafa65f77100641f3
CharlesLiu02 <31979415+CharlesLiu02@users.noreply.github.com>
2019-02-22 23:33:18 -0800

    Delete Credentials.java

--- app/src/main/java/com/hfad/book/Credentials.java
+++
@@ -1,6 +0,0 @@
-package com.hfad.book;
-
-public class Credentials {
-    public static final String API_KEY  = "oraVyCTHSZlSQ4LFRQ5Iow";
-    public static final String SECRET = "AjV79NpKu0Z4E0nglaQn7Dvp7rKI4vPGSp1IzXylQs";
-}
